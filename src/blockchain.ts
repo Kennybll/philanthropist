c361ade9245b93b6d7a2a6e74508226b18b00082
Kenneth Bennett <Kennybll@outlook.com>
2018-02-14 18:51:09 -0500

    Fix genesis

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -33,7 +33,10 @@ class Block {
 const genesisTransaction = {
     'txIns': [{'signature': '', 'txOutId': '', 'txOutIndex': 0}],
     'txOuts': [{
-        'address': '04bfcab8722991ae774db48f934ca79cfb7dd991229153b9f732ba5334aafcd8e7266e47076996b55a14bf9913ee3145ce0cfc1372ada8ada74bd287450313534a',
+        'address': '0447364cb10f4bbb15fa6f3f66243d50246ea794d2b96a378e2047da203f594e5a14fcc500fe4b8d54882efd049b21618b7a835f3c15e4000d5cd9ce580d67e46a',
+        'amount': 50
+    },{
+        'address': '0447364cb10f4bbb15fa6f3f66243d50246ea794d2b96a378e2047da203f594e5a14fcc500fe4b8d54882efd049b21618b7a835f3c15e4000d5cd9ce580d67e46a',
         'amount': 50
     }],
     'id': 'e655f6a5f26dc9b4cac6e46f52336428287759cf81ef5ff10854f69d68f43fa3'
